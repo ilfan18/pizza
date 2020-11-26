c2e8314b03c685215cca15f995a3fe0570616d19
Ilfan <61958987+ilfan18@users.noreply.github.com>
2020-11-26 17:15:46 +0500

    second commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Categories() {
-  return <div></div>;
+function Categories({ items, onClickItem }) {
+  const [activeItem, setActiveItem] = useState(null);
+
+  function handleClickItem(index) {
+    setActiveItem(index);
+  }
+  return (
+    <div className='categories'>
+      <ul>
+        <li
+          className={activeItem === null ? 'active' : ''}
+          onClick={() => handleClickItem(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => {
+            return (
+              <li
+                className={activeItem === index ? 'active' : ''}
+                onClick={() => handleClickItem(index)}
+                key={`${name}_${index}`}
+              >
+                {name}
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 }
+
+export default Categories;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from './Button';
+import Header from './Header';
+import Categories from './Categories';
+import SortPopup from './SortPopup';
+
+export { Button, Header, Categories, SortPopup };
